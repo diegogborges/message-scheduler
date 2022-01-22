5703ce2cc2ce67822700bbdbb723067246ed1685
Diego Gomes Borges <diegogb2013@gmail.com>
2022-01-22 10:58:27 -0300

    first commit

---
+++ src/test/java/com/luizalabs/message/scheduler/MessageSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizalabs.message.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
