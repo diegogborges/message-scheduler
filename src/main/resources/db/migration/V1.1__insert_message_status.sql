4c0390df0f3250ee131557fa488db658fb8cf53a
Diego Gomes Borges <diegogb2013@gmail.com>
2022-01-22 14:37:46 -0300

    feat: create migration to do insert status and type messages

---
+++ src/main/resources/db/migration/V1.1__insert_message_status.sql
@@ -0,0 +1,3 @@
+INSERT INTO `message_status` (`created_at`, `updated_at`, `description`) VALUES (NOW(), NOW(), 'not_sent');
+INSERT INTO `message_status` (`created_at`, `updated_at`, `description`) VALUES (NOW(), NOW(), 'sent');
+INSERT INTO `message_status` (`created_at`, `updated_at`, `description`) VALUES (NOW(), NOW(), 'failed');
---
+++ src/main/resources/db/migration/V1.2__insert_message_type.sql
@@ -0,0 +1,4 @@
+INSERT INTO `message_type` (`created_at`, `updated_at`, `description`) VALUES (NOW(), NOW(), 'sms');
+INSERT INTO `message_type` (`created_at`, `updated_at`, `description`) VALUES (NOW(), NOW(), 'mail');
+INSERT INTO `message_type` (`created_at`, `updated_at`, `description`) VALUES (NOW(), NOW(), 'push');
+INSERT INTO `message_type` (`created_at`, `updated_at`, `description`) VALUES (NOW(), NOW(), 'whatsapp');
