530924f4badeda99cd72b863f5d753718bc0efc4
Diego Gomes Borges <diegogb2013@gmail.com>
2022-01-25 19:12:58 -0300

    chore: change to use lombok

--- src/main/java/com/luizalabs/message/scheduler/v1/model/response/BaseResponseError.java
+++ src/main/java/com/luizalabs/message/scheduler/v1/model/response/BaseResponseError.java
@@ -1,28 +1,14 @@
 package com.luizalabs.message.scheduler.v1.model.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BaseResponseError {
   private Integer code;
   private String message;
   private String details;
-
-  public BaseResponseError() {
-  }
-
-  public BaseResponseError(Integer code, String message, String details) {
-    this.code = code;
-    this.message = message;
-    this.details = details;
-  }
-
-  public Integer getCode() {
-    return this.code;
-  }
-
-  public String getMessage() {
-    return this.message;
-  }
-
-  public String getDetails() {
-    return this.details;
-  }
 }
